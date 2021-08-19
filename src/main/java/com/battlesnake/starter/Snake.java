7829d835f06dadabb4248ce4335dfa6e891a15af
duaneclassen <duane.classen@fcc-fac.ca>
2021-08-19 10:43:57 -0600

    Personalize the Snake

--- src/main/java/com/battlesnake/starter/Snake.java
+++ src/main/java/com/battlesnake/starter/Snake.java
@@ -101,10 +101,10 @@ public class Snake {
         public Map<String, String> index() {         
             Map<String, String> response = new HashMap<>();
             response.put("apiversion", "1");
-            response.put("author", "");           // TODO: Your Battlesnake Username
-            response.put("color", "#888888");     // TODO: Personalize
-            response.put("head", "default");  // TODO: Personalize
-            response.put("tail", "default");  // TODO: Personalize
+            response.put("author", "Moms Spaghetti");
+            response.put("color", "#4A412A");
+            response.put("head", "shades");
+            response.put("tail", "pixel");  
             return response;
         }
 
